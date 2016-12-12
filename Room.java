f55363f95e3641a2f8bdf5f169326baf672e78a6
dewskii <d_dew@post.massbay.edu>
2016-12-12 14:57:26 -0500

    Update Room

--- Room.java
+++ Room.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -18,28 +17,20 @@ public class Room {
   /*attributes*/
   private String roomName;
   private HashMap<String,Door> doorsHash;
-  private Parent parent;
-  private ArrayList<Player> players;
-  //private final Image BACKGROUNDIMAGE;
-  private boolean isStart;
-  Button left;
-  Button right;
-  Label roomLabel;
  
+  private ArrayList<Player> players;
+  
   public Room(){
     roomName = null;
-    parent = null;
-    players = new ArrayList<Player>();
-    //BACKGROUNDIMAGE = null;
-    isStart = false;
+
   }
  
-  public Room(boolean isStart, String roomName/*, Image st*/){
-    this.isStart = isStart;
+  public Room(String roomName){
+ 
     this.roomName = roomName;
     this.doorsHash = new HashMap<String,Door>();
     players = new ArrayList<Player>();
-    //BACKGROUNDIMAGE = st;
+   
   }//indices indicate position of doors [bottom,left,right,top]
  
   public void setDoors(Door[] doors){
@@ -67,9 +58,7 @@ public class Room {
     return roomName;
   }
  
-  public boolean isStart(){
-    return isStart;
-  }
+ 
 
   public void addPlayer(Player p){
     players.add(p);
@@ -85,67 +74,3 @@ public class Room {
   
   
 }
-=======
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
-
-import javafx.geometry.Pos;
-import javafx.scene.Group;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-
-public class Room {
-	private String roomName;
-	private HashMap<String,Door> doorsHash;
-	private Parent parent;
-	
-	
-	public Room(){
-		roomName = null;
-		parent = null;
-	
-		
-	}
-	
-	public Room(String roomName){
-		this.roomName = roomName;
-		this.doorsHash = new HashMap<String,Door>();
-		//BACKGROUNDIMAGE = st;
-	}//indices indicate position of doors [bottom,left,right,top]
-	
-	public void setDoors(Door[] doors){
-		doorsHash.put("BOTTOM", doors[0]);
-		doorsHash.put("LEFT", doors[1]);
-		doorsHash.put("RIGHT", doors[2]);
-		doorsHash.put("TOP", doors[3]);
-	}
-	
-	public Door getDoor(String loc){
-		return doorsHash.get(loc);
-	}
-	
-	public String getAvalibleDoors(){
-		String dr = "";
-		for(Map.Entry<String, Door> entry : doorsHash.entrySet()){
-			String key = entry.getKey();
-			String door = entry.getValue().toString();
-			dr+=key+": "+door+"\n";
-		}
-		return dr;
-	}
-	
-	
-	public String getName(){
-		return roomName;
-	}
-	
-
-
-}
->>>>>>> Stashed changes
