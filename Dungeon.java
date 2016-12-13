e6ad3e0bce921272d4fd3dd0e9a08f10da33437e
msedewitz <msedewitz@rcn.com>
2016-12-12 19:46:32 -0500

    Add files via upload
    Rolled back dungeon and room to davids most recent version, putting my versions back into a seperate tree like i meant to the first time

--- Dungeon.java
+++ Dungeon.java
@@ -1,7 +1,7 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
-import java.util.Map;
 
 import javafx.event.ActionEvent;
 import javafx.scene.Group;
@@ -12,50 +12,41 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 //creates dungeon of rooms on a A-F,1-6 Grid
 //Still need to determine how to "load" in preset map
 public class Dungeon {
+ final double HEIGHT = 350;
+ final double WIDTH = 350;
  Room currentRoom;
  HashMap<String,Room> roomHash;
- int rowNum;
- int colNum;
  GridPane grid;
- Player player1;
- Player player2;
- Player player3;
- Player currPlayer;
+ ArrayList<Player> players;
+ Player currentPlayer;
+ Chaser player1;
+ Runner player2;
+ Runner player3;
+
  
- public Dungeon(int rowNum, int colNum){
+ public Dungeon(){
   roomHash = new HashMap<String,Room>();
-  Room tempRoom;
-  for(int i = 0; i<rowNum; i++){
-   for(int j = 0; j<colNum; j++){
-    tempRoom = new Room(i+","+j);
-    tempRoom.setDoors(colNum, rowNum);
-    roomHash.put((i+","+j),tempRoom);
-   }
-  }
-  currentRoom = roomHash.get("1,1");
-  roomHash.get("0,"+(rowNum-1)).addPlayer(new Player("player1", "0,"+(rowNum-1)));
-  roomHash.get((colNum-1)+",0").addPlayer(new Player("player2",(colNum-1)+",0"));
-  roomHash.get("0,0").addPlayer(new Player("chaser","0,0"));
+  grid = new GridPane();
+  player1 = new Chaser("Player1");
+  player2 = new Runner("Player2");
+  player3 = new Runner("Player3");
+ }
+ 
+ public void changeRoom(Door door){
+  currentRoom.removePlayer(currentPlayer);
+  currentRoom = roomHash.get(door.getName());
+  currentRoom.addPlayer(currentPlayer);
   
  }
  
- public void changeRoom(String key){
-  String newRoom;
-  if (currentRoom.getDoorMap().containsKey(key)){
-    newRoom = currentRoom.getDoor(key).name();
-    if (currentRoom.hasPlayer()){
-      roomHash.get(newRoom).addPlayer(currentRoom.getPlayer());
-      currentRoom.removePlayer();}
-    currentRoom = roomHash.get(newRoom);
-  }
- }
- 
- public HashMap<String, Room> getMap(){
-   return roomHash;
+ public Player getCurrentPlayer(){
+  return currentPlayer;
  }
  
  public Room getCurrentRoom(){
@@ -66,17 +57,43 @@ public class Dungeon {
   return currentRoom.getName();
  }
  
- //public GridPane getGrid(){
-  //return grid;
- //}
+ //initializes rooms from text file, doors point to available rooms
  
- public String toString(){
-   String str = "";
-   for(Map.Entry<String, Room> entry : roomHash.entrySet()){
-     str+="["+entry.toString()+"]\n";}
-   return str;
+ public void initialize(){
+  System.out.println("INITIALIZE");
+  Room tempRoom;
+  Door[] door = new Door[4];
+  int row = 9;
+  int col = 9;
+  for(int i = 0; i<row; i++){
+   for(int j = 0; j<col; j++){
+    int r = i;
+    int c = j;
+    tempRoom = new Room(i+","+j);
+    door[0] = new Door((r+1)+","+(c));
+    door[1] = new Door((r)+","+(c-1));
+    door[2] = new Door((r)+","+(c+1));
+    door[3] = new Door((r-1)+","+(c));
+    tempRoom.setDoors(door);
+    roomHash.put(tempRoom.getName(), tempRoom);
+    tempRoom.setRoom(WIDTH/row, HEIGHT/col);
+    grid.add(tempRoom.getRoomShape(), c, r);
+   }      
+    }
+  currentRoom = roomHash.get((int)Math.floor(Math.random()*8)+","+(int)Math.floor(Math.random()*8));
+  currentPlayer = player1;
+  currentRoom.addPlayer(player1);
+  
+  }
+    
+  
+ 
+ 
+ public GridPane getGrid(){
+  return grid;
  }
-   
+ 
+ 
  
  
 
